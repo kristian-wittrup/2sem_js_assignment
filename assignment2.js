2b859dad7d199db198cd1d8837be51fd46911e89
kristian-wittrup <kw@easv.dk>
2021-02-03 09:10:38 +0100

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -61,43 +61,46 @@ function fahrenheitToCelsius(fahrenheit) {
 //
 // The Calculator assignment
 //
-function squareNumber(num) {
-    var squaredNumber = num * num;
-    console.log('The result of squaring the number ' + num + ' is ' + squaredNumber);
-    return squaredNumber;
-}
-
-squareNumber(3);
-
 function halfOf(num) {
-    var half = num / 2;
+    let half = num / 2;
     console.log('Half of ' + num + ' is ' +  half);
     return half;
 }
 
-halfOf(5);
+//halfOf(5);
+
+function squareNumber(num) {
+    let squaredNumber = num * num;
+    console.log('The result of squaring the number ' + num + ' is ' + squaredNumber);
+    return squaredNumber;
+}
+
+//squareNumber(3);
+
+
+function areaOfCircle(radius) {
+    let area = Math.PI * squareNumber(radius);
+    //console.log('The area of circle with radius ' + radius + ' is ' + area);
+    console.log(`The area of circle with radius  ${radius}  is ${radius}`);
+    return area;
+}
+
+//areaOfCircle(2);
 
 function percentOf(num1, num2) {
-    var percent = (num1/num2) * 100;
+    let percent = (num1/num2) * 100;
     console.log(num1 + ' is ' + percent + '% of ' + num2);
     return percent;
 }
 
-percentOf(5, 10);
+//percentOf(5, 10);
 
-function areaOfCircle(radius) {
-    var area = Math.PI * squareNumber(radius);
-    console.log('The area of circle with radius ' + radius + ' is ' + area);
-    return area;
-}
-
-areaOfCircle(2);
 
 function doCrazyStuff(num) {
-    var half    = halfOf(num);
-    var squared = squareNumber(half);
-    var area    = areaOfCircle(squared);
-    var result  = percentOf(squared, area);
+    let half    = halfOf(num);
+    let squared = squareNumber(half);
+    let area    = areaOfCircle(squared);
+    let result  = percentOf(squared, area);
 }
 
 doCrazyStuff(5);

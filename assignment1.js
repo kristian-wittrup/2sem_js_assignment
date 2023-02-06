37f326f3b1203f676969af1a9c3e4cb764666eeb
kristian-wittrup <kw@easv.dk>
2023-02-06 07:58:10 +0100

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,40 +1,41 @@
 //assignment 1
 //exercise 1;
 
-var numKids  = 2;
-var partner  = 'That girl';
-var location = 'Sumplace warm';
-var jobTitle = 'leisurable larry';
+let numKids  = 2;
+let partner  = 'That girl';
+let location = 'Sumplace warm';
+let jobTitle = 'leisurable larry';
 
-var future = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' +
+let future = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' +
    partner + ' ' + ' with ' + numKids + ' kids.';
 console.log(future);
 
 //exercise 2;
-var birthYear = 1982;
-var futureYear  = 2030;
-var age  = futureYear - birthYear;
-console.log('I will be either ' + age + ' or ' + (age - 1));
+let birthYear = 1982;
+let futureYear  = 2030;
+let age  = futureYear - birthYear;
+console.log(`I will be either ${age} or ${age - 1}`);
 
 //exercise 3;
-var age = 34;
-var maxAge = 100;
-var numPerDay = 2;
-var totalNeeded = (numPerDay * 365) * (maxAge - age);
-var message = 'You will need ' + totalNeeded + ' cups of coffey to last you until the ripe old age of ' + maxAge;
+let currentAge = 34;
+let maxAge = 100;
+let numPerDay = 2;
+let totalNeeded = (numPerDay * 365) * (maxAge - currentAge);
+let message = `You will need ${totalNeeded} cups of coffey to last you until the ripe old age of ${maxAge}`;
 console.log(message);
 
+
 //exercise 4;
-var radius = 5;
-var circumference = Math.PI * 2*radius;
-console.log("The circumference is " + circumference);
-var area = Math.PI * radius*radius;
-console.log("The area is " + area);
+let radius = 5;
+let circumference = Math.PI * 2*radius;
+console.log(`The circumference is ${circumference}`);
+let area = Math.PI * radius*radius;
+console.log(`The area is ${area}`);
 
 //exercise 5;
-var celsius = 25;
-var celsiusInF = (celsius*9)/5 + 32;
-console.log(celsius + '°C is ' + celsiusInF + '°F');
-var fahrenheit = 20;
-var fahrenheitInC = ((fahrenheit - 32)*5)/9;
-console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+let celsius = 25;
+let celsiusInF = (celsius*9)/5 + 32;
+console.log(`${celsius}°C is ${celsiusInF}°F`);
+let fahrenheit = 20;
+let fahrenheitInC = ((fahrenheit - 32)*5)/9;
+console.log(`${fahrenheit}°F is ${fahrenheitInC}°C`);
